e16060dbd457f3c1d46f337de9fbc71c0879c3c5
unknown <roshansingh1397@gmail.com>
2024-04-28 17:24:06 +0530

    sending some data through add Attribute for dynamic page

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/home")
-	public String home() {
-		return "Welcome.html";
+	public String home(Model page) {
+		page.addAttribute("userName","katty");
+		page.addAttribute("color","blue");
+		return "home.html";
 	}
 }
